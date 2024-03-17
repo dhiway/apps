6153c51f8454e0e0ff177a14bdea5b16b4470d5d
Satish Mohan <54302767+smohan-dw@users.noreply.github.com>
2024-03-17 08:42:40 +0530

    cord spec configuration

---
+++ packages/apps-config/src/api/spec/cord.ts
@@ -0,0 +1,9 @@
+/* eslint-disable header/header */
+// Copyright 2017-2023 @polkadot/apps-config authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import type { OverrideBundleDefinition } from '@polkadot/types/types';
+
+import { typesBundle } from '@cord.network/type-definitions';
+
+export default typesBundle.spec as Record<string, OverrideBundleDefinition>;
