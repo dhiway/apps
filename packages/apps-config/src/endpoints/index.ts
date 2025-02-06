53ff98da7be339a1d29859af3484d4cee746c3bb
Shree Vatsa N <i.mnshreevatsa@gmail.com>
2025-02-06 14:58:53 +0530

    app-config: Add custom endpoint dropdown (#57)
    Signed-off-by: Shreevatsa N <vatsa@dhiway.com>

--- packages/apps-config/src/endpoints/index.ts
+++ packages/apps-config/src/endpoints/index.ts
@@ -5,7 +5,6 @@ import type { TFunction, TOptions } from '../types.js';
 import type { LinkOption } from './types.js';
 
 import { createCustom, createDev, createOwn } from './development.js';
-import { testRelayPaseo } from './testingRelayPaseo.js';
 import { expandEndpoints } from './util.js';
 
 export { CUSTOM_ENDPOINT_KEY } from './development.js';
@@ -42,6 +41,17 @@ export function createWsEndpoints (t: TFunction = defaultT, firstOnly = false, w
       value: ''
     },
     ...expandEndpoints(t, prodChains, firstOnly, withSort),
+    ...expandEndpoints(t, stagingChains, firstOnly, withSort),
+    {
+      isDevelopment: true,
+      isDisabled: false,
+      isHeader: true,
+      isSpaced: true,
+      text: t('rpc.header.dev', 'Development', { ns: 'apps-config' }),
+      textBy: '',
+      ui: {},
+      value: ''
+    },
     ...createDev(t),
     ...createOwn(t)
   ].filter(({ isDisabled }) => !isDisabled);
